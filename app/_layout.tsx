1da3b596d59d2ad5e0ae069be105464e4e31f04e
Luke Spivak <147668369+Lukespiv@users.noreply.github.com>
2025-03-12 16:55:56 -0600

    Add files via upload

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,39 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import { Ionicons, Feather, FontAwesome } from "@expo/vector-icons";
 
-export default function Layout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+const Layout = () => 
+  {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: "black",
+          borderTopWidth: 0,
+          height: 60,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="feed"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="home-outline" size={size} color="white" />
+          ),
+          headerShown: false,
+        }}
+      />
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="search-outline" size={size} color="white" />
+          ),
+          headerShown: false,
+        }}
+      />
+    </Tabs>
+  )
 }
+
+export default Layout
